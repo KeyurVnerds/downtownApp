a31d1a30473d9f6d131e962b9e3ef0163417a19c
Aaron Marsh <aaronmarsh755@gmail.com>
2020-09-11 23:29:05 -0400

    Initial Commit

---
+++ DowntownApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DowntownApp
+//
+//  Created by Aaron Marsh on 9/11/20.
+//  Copyright © 2020 Aaron Marsh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
